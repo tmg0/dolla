ece7197ff71e77a366ad7ae0f32f5d194e0a4d57
Tamago <zekun.jin@foxmail.com>
2024-08-14 16:28:23 +0800

    fix: ensure dist bin dir

--- packages/rag/scripts/build.ts
+++ packages/rag/scripts/build.ts
@@ -1,5 +1,6 @@
 async function run() {
-  Bun.spawn(['gcc', '-o', './bin/libsqlite3.dylib', './bin/sqlite3.c', '-dynamiclib'], { stdout: 'inherit' })
+  Bun.spawnSync(['mkdir', './dist/bin'], { stdout: 'inherit' })
+  Bun.spawn(['gcc', '-o', './dist/bin/libsqlite3.dylib', './bin/sqlite3.c', '-dynamiclib'], { stdout: 'inherit' })
   Bun.spawn(['bun', 'build', './src/index.ts', '--compile', '--outfile', './dist/rag'], { stdout: 'inherit' })
 }
 
